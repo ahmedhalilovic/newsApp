35746da1388cb9e45cffe45deb87e1d3cfbcee63
Ahmed <ahmeddhalilovic@gmail.com>
2024-08-18 17:57:11 +0200

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Ahmed Halilovic on 18. 8. 2024..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
